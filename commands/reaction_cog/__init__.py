77ab5743ec7b99793f163e116b753f6d4aea9649
Muhamed Blogs <kg7221996@gmail.com>
2025-06-28 22:55:20 +0300

    reactions to a message in the community chat

--- commands/reaction_cog/__init__.py
+++ commands/reaction_cog/__init__.py
@@ -2,6 +2,7 @@ from disnake.ext import commands
 from .music import ReactionMusic
 from .youtube import ReactionYouTube
 from .telegram import ReactionTelegram
+from .chat import RandomReactor
 
 
 
@@ -9,4 +10,4 @@ def setup(bot: commands.Bot):
     bot.add_cog(ReactionMusic(bot))
     bot.add_cog(ReactionYouTube(bot))
     bot.add_cog(ReactionTelegram(bot))
-
+    bot.add_cog(RandomReactor(bot))
---
+++ commands/reaction_cog/chat.py
@@ -0,0 +1,30 @@
+import disnake
+from disnake.ext import commands
+import random
+
+from BANNED_FILES.config import CHAT_CHANNEL_ID, Probability_Reaction, CHAT_Reaction
+
+class RandomReactor(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.target_channel_id = CHAT_CHANNEL_ID
+
+    @commands.Cog.listener()
+    async def on_message(self, message: disnake.Message):
+        if (
+            message.channel.id != self.target_channel_id
+            or message.author.bot
+            or not message.content.strip()
+        ):
+            return
+
+        if random.randint(1, 100) <= Probability_Reaction:
+            try:
+                emoji_id = random.choice(CHAT_Reaction)
+                emoji = self.bot.get_emoji(emoji_id)
+                if emoji is None:
+                    print(f"Эмодзи с ID {emoji_id} не найден в кэше бота")
+                    return
+                await message.add_reaction(emoji)
+            except disnake.HTTPException as e:
+                print(f"❌ Ошибка добавления реакции: {e}")
