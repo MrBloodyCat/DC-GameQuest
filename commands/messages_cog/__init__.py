80b40bf1b3b0b872ea812b35eb0996f5f6bd90a4
Muhamed Blogs <kg7221996@gmail.com>
2025-06-28 22:56:58 +0300

    promotional messages from the bot for the community

--- commands/messages_cog/__init__.py
+++ commands/messages_cog/__init__.py
@@ -2,9 +2,11 @@ from disnake.ext import commands
 from .responder import DMResponder
 from .greeting import GreetingResponder
 from .handler import WelcomeHandler
+from .promo import AutoPromo
 
 
 def setup(bot: commands.Bot):
     bot.add_cog(DMResponder(bot))
     bot.add_cog(GreetingResponder(bot))
     bot.add_cog(WelcomeHandler(bot))
+    bot.add_cog(AutoPromo(bot))
